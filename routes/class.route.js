ca1e5fb11ab46988d49cb69fd871341decc47396
HoangPham <hoapha1009@gmail.com>
2020-05-10 20:52:26 +0700

    Build-class: Finish index, delete - unfinished create, view

--- routes/class.route.js
+++ routes/class.route.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const classController = require('../controllers/class.controller');
 
 router.get('/', classController.render);
+router.get('/delete/:id', classController.delete);
 
 module.exports = router;
